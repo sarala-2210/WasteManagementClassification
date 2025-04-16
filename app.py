79a95305ac3bed2c16c5d9dfc45ee26b875f1364
Sunkara Sarala <52791971+sarala-2210@users.noreply.github.com>
2025-04-15 22:12:33 -0400

    app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ uploaded_file = st.file_uploader("📤 Upload an image", type=["jpg", "jpeg", "p
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
-    st.image(img, caption="Uploaded Image", use_column_width=True)
+    st.image(img, caption="Uploaded Image", use_container_width=True)
 
     input_image = preprocess_single_image(img)
 
